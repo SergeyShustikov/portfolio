a4bb1ac657df2628e4e90f129f030b194808fecd
Sergey Shustikov <pandarium.shustikov@gmail.com>
2023-03-30 07:39:37 +0300

    Added drawer to mobile version

--- lib/widgets/mobile_drawer.dart
+++ lib/widgets/mobile_drawer.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/theming/palette.dart';
+import 'package:portfolio/widgets/content_container.dart';
 
 class MobileDrawer extends StatelessWidget {
   const MobileDrawer({super.key});
@@ -6,12 +10,42 @@ class MobileDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Palette.backgroundColor,
       shape: ContinuousRectangleBorder(
         borderRadius: BorderRadius.circular(28.0),
       ),
-      child: Container(
-        color: Colors.red,
+      child: Column(
+        children: [
+          ContentContainer(
+            height: kToolbarHeight + 14,
+            child: Center(child: Text('Go to :', style: GoogleFonts.ubuntu(fontSize: 18, fontWeight: FontWeight.bold))),
+          ),
+          Container(
+            height: 1,
+            color: Colors.white,
+          ),
+          Container(
+            color: Palette.backgroundColor,
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                ListTile(
+                  title: const Text('About me'),
+                  onTap: () {
+                    context.go('/about');
+                  },
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Projects'),
+                  onTap: () {
+                    context.go('/projects');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
